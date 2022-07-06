33893bc3d64a5e4346b61e2544c931c1eca98244
James Byrne <james.c.byrne1994@gmail.com>
2022-07-06 16:30:47 +0100

    added name and age

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -3,11 +3,9 @@ let firstNumber = 10
 
 // TODO: Set the value of firstNumber below so the tests pass
 
-
 // TODO: Change the code below so that the tests pass
 const secondNumber = 42 // edit this value
 firstNumber = 20
-
 
 // do not edit the exported object.
 module.exports = {
