cddf94e292fca13e50ea2682dca60efaeb52b2f0
James Byrne <james.c.byrne1994@gmail.com>
2022-07-06 15:58:35 +0100

    added values to first and second number

--- src/variables/assignment.js
+++ src/variables/assignment.js
@@ -1,11 +1,13 @@
 // do not edit this
 let firstNumber = 10
-firstNumber = 0
 
 // TODO: Set the value of firstNumber below so the tests pass
 
+
 // TODO: Change the code below so that the tests pass
-const secondNumber = 0 // edit this value
+const secondNumber = 42 // edit this value
+firstNumber = 20
+
 
 // do not edit the exported object.
 module.exports = {
