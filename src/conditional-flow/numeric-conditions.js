78996734ccdb3d528ed35341e3801e022eb94501
James Byrne <james.c.byrne1994@gmail.com>
2022-07-08 16:44:35 +0100

    used if statments, need to complete for loop

--- src/conditional-flow/numeric-conditions.js
+++ src/conditional-flow/numeric-conditions.js
@@ -7,10 +7,21 @@ const ARRAY_TWO = ['Hello', 'Conditions']
 // or true if it is empty
 let answerOne
 
+if (ARRAY_ONE.length >= 1){
+    answerOne = false
+} else {
+    answerOne = true
+}
+
 // Use conditional statements to set answerTwo to false if ARRAY_TWO is not empty
 // or true if it is empty
 let answerTwo
 
+if (ARRAY_ONE.length < 1){
+    answerTwo = false
+} else {
+    answerTwo = true
+}
 
 
 const NUM_ONE = 92837
@@ -18,7 +29,11 @@ const NUM_TWO = 92827
 
 // Use conditional statements to set answerThree to true if NUM_ONE is more than NUM_TWO
 let answerThree
-
+if (NUM_ONE > NUM_TWO ){
+    answerThree = true
+} else {
+    answerThree = false
+}
 
 
 const NUMBERS = [67, 2039, 17, 501, 98, 139, 21]
@@ -26,8 +41,6 @@ const NUMBERS = [67, 2039, 17, 501, 98, 139, 21]
 // Use a combination of a loop and conditional statements to set answerFour
 // to the lowest number in the NUMBERS array
 let answerFour
-
-
 
 
 // Don't edit the code below this line
