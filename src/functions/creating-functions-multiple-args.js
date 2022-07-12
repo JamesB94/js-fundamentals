5806236da8215880246a5e0266d6abccdf8538b7
James Byrne <james.c.byrne1994@gmail.com>
2022-07-12 16:21:38 +0100

    added functions

--- src/functions/creating-functions-multiple-args.js
+++ src/functions/creating-functions-multiple-args.js
@@ -10,6 +10,16 @@
 //
 // TODO: write code below
 
+let numOutput = []
+
+function twoNums(lower,upper){
+ for(let i = lower; i <= upper; i++){
+   numOutput.push(i)
+ }
+ return numOutput
+}
+
+
 // define a function that takes two arguments: a string and a number.
 // The function should return the same string but in upper case with exclamation
 // marks appended to the end. The number of exclamation marks should be
@@ -22,8 +32,24 @@
 //
 // TODO: write code below
 
+function upperNums(string,nums){
+  let text = "!"
+  let exclamation = text.repeat(nums)
+  let upperCase = string.toUpperCase()
+  return upperCase + exclamation
+}
+
+// function upperNums(string,nums){
+//   let upperCase = string.toUpperCase()
+
+//   for(let i=0; i <= nums.length; i++){
+//     let upperPlusNums = upperCase + "!"
+//   }
+//   return upperPlusNums 
+// }
+
 // change the exported value to be the name of the function you defined
 module.exports = {
-  a: undefined, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
-  b: undefined // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
+  a: twoNums, // change undefined to be the name of the function defined to create the range of numbers (the first todo)
+  b: upperNums // change undefined to be the name of the function defined to return the string with exclamations (the second todo)
 }
